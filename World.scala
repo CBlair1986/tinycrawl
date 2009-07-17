fea8a30410c0d210f74b9348ae8917f607b690ea
Christopher Blair <CBlair1986@gmail.com>
2009-07-17 14:11:09 -0400

    Created stubs in World for generation, and ...
    added WorldView

--- World.scala
+++ World.scala
@@ -1,23 +1,54 @@
 package tinycrawl.model {
+  /**
+    Contains conveniences, such as world generation
+    */
+  object World {
+    /**
+      Generates a randomized world, with <param>num</param> nodes.
+      */
+    // TODO
+    def random(num: Int): World = {
+      new World
+    }
+  }
+
+  /**
+    Represents a world that agents can move around in
+    */
   class World {
     import collection.mutable.Set
 
     var nodes: Set[Node] = Set()
     var edges: Set[Edge] = Set()
 
+    /**
+      Adds a node of the <param>terrainType</param> type at
+      <param>position</param> position
+      */
     def addNode(terrainType: Symbol, position: Position): Unit = {
       nodes += new Node(terrainType, position, ID.next)
     }
 
+    /**
+      Removes the specified node from the world
+      */
     def removeNode(node: Node): Node = {
       nodes -= node
       node
     }
 
+    /**
+      Finds a node, given an id
+      */
     def findNodeByID(id: Int): Option[Node] = {
       nodes find { case Node(_, _, anID) => id == anID }
     }
 
+    /**
+      Creates a connection between two nodes, checking first to see if the
+      nodes actually exist
+      */
+    // TODO
     def connect(startNode: Node, endNode: Node): Unit = {
       if ((nodes contains startNode) && (nodes contains endNode)) {
         println("Contains")
---
+++ WorldView.scala
@@ -0,0 +1,7 @@
+package tinycrawl.view {
+  import model._
+  import swing._
+  import event._
+
+  class WorldView(world: World) extends Component { }
+}
